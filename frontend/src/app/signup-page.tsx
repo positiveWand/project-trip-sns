e8058b72061e43bbc6a33f3e9f2a8c436a263233
positiveWand <singbhang@gmail.com>
2025-02-08 16:34:18 +0900

    feat/frontend: 회원가입 페이지

--- frontend/src/app/signup-page.tsx
+++ frontend/src/app/signup-page.tsx
@@ -1,21 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@/styles/index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/styles/index.css';
+import { SignupForm } from '@/components/signup-form';
+import { Toaster } from '@/components/ui/toaster';
+import Page from '@/components/layout/page';
+import { Header } from '@/components/layout/header';
+import Main from '@/components/layout/main';
+import initApp from '@/init';
+
+await initApp();
 
 function SignupPage() {
-    return (
-        <div>
-        회원가입 페이지
+  return (
+    <Page>
+      <Header />
+      <Main className='w-full h-full flex items-center justify-center'>
+        <div className='w-full max-w-sm'>
+          <SignupForm />
         </div>
-    )
+      </Main>
+      <Toaster />
+    </Page>
+  );
 }
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <SignupPage />
-    </StrictMode>,
-)
-  
-  
-export default SignupPage
-  
+  <StrictMode>
+    <SignupPage />
+  </StrictMode>,
+);
+
+export default SignupPage;
