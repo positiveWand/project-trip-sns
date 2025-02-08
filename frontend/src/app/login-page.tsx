f5448d92335086af48485f9a4528705d8d36b995
positiveWand <singbhang@gmail.com>
2025-02-08 16:09:06 +0900

    feat/frontend: 로그인 페이지

--- frontend/src/app/login-page.tsx
+++ frontend/src/app/login-page.tsx
@@ -1,21 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@/styles/index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@/styles/index.css';
+import { LoginForm } from '@/components/login-form';
+import { Toaster } from '@/components/ui/toaster';
+import Page from '@/components/layout/page';
+import { Header } from '@/components/layout/header';
+import Main from '@/components/layout/main';
+import initApp from '@/init';
+
+await initApp();
 
 function LoginPage() {
-    return (
-        <div>
-        로그인 페이지
+  return (
+    <Page>
+      <Header />
+      <Main className='w-full h-full flex items-center justify-center'>
+        <div className='w-full max-w-sm'>
+          <LoginForm />
         </div>
-    )
+      </Main>
+      <Toaster />
+    </Page>
+  );
 }
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <LoginPage />
-    </StrictMode>,
-)
-  
-  
-export default LoginPage
-  
+  <StrictMode>
+    <LoginPage />
+  </StrictMode>,
+);
+
+export default LoginPage;
