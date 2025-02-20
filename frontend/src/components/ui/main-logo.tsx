e918fb2245a7cc8c7c3f14ba721dfc2978699b07
positiveWand <singbhang@gmail.com>
2025-02-20 13:12:05 +0900

    feat/frontend: 메인 로고 변경

--- frontend/src/components/ui/main-logo.tsx
+++ frontend/src/components/ui/main-logo.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import logoUrl from '@/assets/beacon.png'
+import logoUrl from '@/assets/map.png'
 import { cn } from "@/lib/utils"
 
 export interface MainLogoProps
@@ -13,7 +13,7 @@ const MainLogo = React.forwardRef<HTMLAnchorElement, MainLogoProps>(
     return (
         <a href={href} className={cn('flex items-center', className)} ref={ref} {...props}>
             <img src={logoUrl} alt="메인 로고 이미지" className='w-8 mr-1' />
-            <h1 className='font-extrabold text-3xl align-middle'>BMS</h1>
+            <h1 className='font-extrabold text-3xl align-middle'>TOURIN</h1>
         </a>
     )
 }
