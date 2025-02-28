8e8304a450abe5401e458bd15bdb667863344002
positiveWand <singbhang@gmail.com>
2025-02-28 23:14:30 +0900

    feat/frontend: 북마크, 관광지 후기 관련 request 모듈
    - 관광지 후기 추가/삭제/변경/조회
    - 북마크 추가/삭제

---
+++ frontend/src/lib/requests/request-bookmark.ts
@@ -0,0 +1,28 @@
+import { Response, envelopeApiClient, defaultErrorHandler } from './request';
+
+export interface Bookmark {
+  userId: string;
+  tourSpotId: string;
+}
+
+type RequestBookmarkError = never;
+
+export function requestPostUserBookmark(userId: string, tourSpotId: string) {
+  return envelopeApiClient
+    .post<Response<Bookmark, RequestBookmarkError>>(`/users/${userId}/bookmarks`, {
+      tourSpotId: tourSpotId,
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch(defaultErrorHandler);
+}
+
+export function requestDeleteUserBookmark(userId: string, tourSpotId: string) {
+  return envelopeApiClient
+    .delete<Response<null, RequestBookmarkError>>(`/users/${userId}/bookmarks/${tourSpotId}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch(defaultErrorHandler);
+}
---
+++ frontend/src/lib/requests/request-tour-spot-review.ts
@@ -0,0 +1,74 @@
+import { defaultErrorHandler, envelopeApiClient, Response } from './request';
+
+export interface TourSpotReview {
+  id: string;
+  tourSpotId: string;
+  userId: string;
+  content: string;
+  likes: number;
+  time: string;
+}
+
+export interface TourSpotReviewLike {
+  userId: string;
+  tourSpotReviewId: string;
+  liked: boolean;
+}
+
+export type RequestTourSpotError = never;
+
+export function requestPostTourSpotReview(tourSpotId: string, userId: string, content: string) {
+  return envelopeApiClient
+    .post<Response<TourSpotReview, RequestTourSpotError>>(`/tour-spots/${tourSpotId}/reviews`, {
+      userId: userId,
+      content: content,
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch(defaultErrorHandler);
+}
+
+export function requestDeleteTourSpotReview(tourSpotReviewId: string) {
+  return envelopeApiClient
+    .delete<Response<null, RequestTourSpotError>>(`/tour-spot-reviews/${tourSpotReviewId}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch(defaultErrorHandler);
+}
+
+export function requestPutTourSpotReviewLike(
+  userId: string,
+  tourSpotReviewId: string,
+  liked: boolean,
+) {
+  return envelopeApiClient
+    .put<Response<TourSpotReviewLike, RequestTourSpotError>>(
+      `/tour-spot-reviews/${tourSpotReviewId}/likes`,
+      {
+        userId: userId,
+        liked: liked,
+      },
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch(defaultErrorHandler);
+}
+
+export function requestGetTourSpotReviewLikes(userId: string, tourSpotReviewIds: string[]) {
+  return envelopeApiClient
+    .get<Response<TourSpotReviewLike[], RequestTourSpotError>>(
+      `/users/${userId}/tour-spot-reviews/likes`,
+      {
+        params: {
+          tourSpotReviewIds: tourSpotReviewIds,
+        },
+      },
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch(defaultErrorHandler);
+}
