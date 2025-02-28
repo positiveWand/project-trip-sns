8875bcc23536cbf452c4ecea7a43ae28104ffb87
positiveWand <singbhang@gmail.com>
2025-03-01 01:22:58 +0900

    feat/frontend: useProxyState 커스텀 훅
    전달된 데이터를 프록시하는 React state를 유지하는 커스텀 훅.
    추적하는 프록시 타겟과는 별개로 상태 갱신 가능, transform으로 데이터 변환도 가능.
    
    예상 유스케이스: 서버 데이터 프록시

---
+++ frontend/src/hooks/use-proxy-state.ts
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export async function IDENTITY_TRANSFORM<T>(value: T) {
+  return value;
+}
+
+export function useProxyState<T, U>(
+  state: T,
+  transform: (state: T) => Promise<U | null>,
+): [U | null, React.Dispatch<React.SetStateAction<U | null>>] {
+  const [proxyState, setProxyState] = React.useState<U | null>(null);
+
+  React.useEffect(() => {
+    transform(state)
+      .then((transformedState) => setProxyState(transformedState))
+      .catch(() => setProxyState(null));
+  }, [state]);
+
+  return [proxyState, setProxyState];
+}
