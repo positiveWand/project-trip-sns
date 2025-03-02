1defc7dae041f76b504720c00c6a3d71c99e4400
positiveWand <singbhang@gmail.com>
2025-03-02 18:41:43 +0900

    etc/frontend: vite dev 서버 설정 변경
    - URL 하위 경로도 인식할 수 있도록 미들웨어 추가

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react(),
+    (() => {return {
+      name: 'configure-server',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.startsWith('/map/')) {
+            req.url = '/map/index.html';
+          } else if(req.url?.startsWith('/social/')) {
+            req.url = '/social/index.html';
+          }
+          next();
+        })
+      }
+    }})()
   ],
   resolve: {
     alias: {
