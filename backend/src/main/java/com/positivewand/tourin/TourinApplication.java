af697669b1739c070282ce9a2fef18a0b3b4122d
positiveWand <singbhang@gmail.com>
2025-03-08 19:27:49 +0900

    init/backend: Spring 프로젝트

---
+++ backend/src/main/java/com/positivewand/tourin/TourinApplication.java
@@ -0,0 +1,13 @@
+package com.positivewand.tourin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourinApplication.class, args);
+	}
+
+}
