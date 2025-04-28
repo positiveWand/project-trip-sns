81e68313c4853c73be75aac29f82e1d2117c56af
positiveWand <singbhang@gmail.com>
2025-04-28 23:35:11 +0900

    refactor/backend: "Response" 오타 고치기

--- backend/src/main/java/com/positivewand/tourin/web/aop/PageResponseAdvice.java
+++ backend/src/main/java/com/positivewand/tourin/web/aop/PageResponseAdvice.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 @RestControllerAdvice
-public class PageResonseAdvice implements ResponseBodyAdvice<Object> {
-
-
+public class PageResponseAdvice implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
         return Page.class.isAssignableFrom(returnType.getParameterType());
