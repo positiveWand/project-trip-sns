7627797301ab92979b898c3a6f5bdd8a6cede109
positiveWand <singbhang@gmail.com>
2025-08-15 22:44:34 +0900

    test/backend: 트렌드 추천 관련 테스트
    - TrendService, TrendAspect 클래스에 대한 고립 테스트
    - 새로 추가된 API(GET /api/recommendations/trend)에 대한 테스트

---
+++ backend/src/test/java/com/positivewand/tourin/web/aop/TrendAspectTest.java
@@ -0,0 +1,138 @@
+package com.positivewand.tourin.web.aop;
+
+import com.positivewand.tourin.domain.auth.CustomUserDetails;
+import com.positivewand.tourin.domain.auth.CustomUserDetailsService;
+import com.positivewand.tourin.domain.recommendation.TrendService;
+import com.positivewand.tourin.domain.tourspot.TourSpotService;
+import com.positivewand.tourin.domain.tourspot.dto.TourSpotDto;
+import com.positivewand.tourin.domain.user.BookmarkService;
+import com.positivewand.tourin.domain.user.dto.BookmarkDto;
+import com.positivewand.tourin.web.tourspot.TourSpotController;
+import com.positivewand.tourin.web.user.UserController;
+import com.positivewand.tourin.web.user.request.AddBookmarkRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.*;
+
+import static com.positivewand.tourin.web.aop.TrendAspect.BOOKMARK_SCORE;
+import static com.positivewand.tourin.web.aop.TrendAspect.VISIT_SCORE;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class TrendAspectTest {
+    @Autowired
+    TourSpotController tourSpotController;
+    @Autowired
+    UserController userController;
+    @MockitoBean
+    TrendService trendService;
+    @MockitoBean
+    CustomUserDetailsService userDetailsService;
+    @MockitoBean
+    BookmarkService bookmarkService;
+    @MockitoBean
+    TourSpotService tourSpotService;
+
+    @Test
+    void AOP를_활용해_트렌드_정보를_정확히_수집한다() throws Exception {
+        // given - 10명의 사용자, 100개의 관광지
+        Set<String> usernames = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            usernames.add("user"+i);
+        }
+        List<Long> tourSpotIds = new ArrayList<>();
+        for (long i = 0; i < 100; i++) {
+            tourSpotIds.add(i);
+        }
+
+        Map<Long, Long> collected = new HashMap<>();
+        
+        // Controller가 호출하는 Service 모킹
+        when(tourSpotService.findTourSpot(anyLong())).thenAnswer(invocation -> {
+            long tourSpotId = invocation.getArgument(0, Long.class);
+
+            if (!tourSpotIds.contains(tourSpotId))
+                throw new NoSuchElementException();
+
+            return new TourSpotDto(tourSpotId, null, null, 1.0, 1.0, null, null, null, null);
+        });
+
+        when(bookmarkService.addBookmark(anyString(), anyLong())).thenAnswer(invocation -> {
+            String username = invocation.getArgument(0, String.class);
+            long tourSpotId = invocation.getArgument(1, Long.class);
+
+            if (!usernames.contains(username) || !tourSpotIds.contains(tourSpotId))
+                return new NoSuchElementException();
+
+            return new BookmarkDto("", tourSpotId, new TourSpotDto(tourSpotId, null, null, 1.0, 1.0, null, null, null, null));
+        });
+        
+        // 테스트 관심사는 TrendAspect이기 때문에 그것이 의존하는 TrendService도 모킹
+        doAnswer(invocation -> {
+            long tourSpotId = invocation.getArgument(0, Long.class);
+            int delta = invocation.getArgument(1, Integer.class);
+
+            if (!collected.containsKey(tourSpotId))
+                collected.put(tourSpotId, 0L);
+            collected.put(tourSpotId, collected.get(tourSpotId) + delta);
+            return null;
+        }).when(trendService).incrementTrendScore(anyLong(), anyInt());
+
+        // 20번의 무작위 테스트 세션을 통과해야한다
+        // 세션마다 트렌드 점수에 반영이 되는 경우(요청 정상 처리)와 안되는 경우(처리 중 예외 발생)가 섞여 있다
+        // AOP를 이용해 처리가 정상적으로 완료된 요청만 트렌드 점수에 반영되고, 비정상 처리된 요청은 트렌드 점수에 반영되지 않아야 한다
+        // 확률적 프로세스를 거쳐 기대하는 순위와 AOP로 집계된 점수 순위가 일치하면 수집이 정상적으로 됐다고 생각할 수 있다
+        for (int iter = 0; iter < 20; iter++) {
+            collected.clear();
+
+            Map<Long, Long> expected = new HashMap<>();
+            
+            for (int i = 0; i < 1000; i++) {
+                // 전체 중 20% - 예외 발생하여 집계되지 않음
+                // 전체 중 80% - 정상 처리되어 집계됨
+                if (Math.random() < 0.2) {
+                    try {
+                        tourSpotController.getTourSpot(2000L);
+                    } catch (Exception e) {}
+                } else {
+                    // 무작위 관광지 선정
+                    long id = (long) (Math.random() * 99) + 1;
+                    tourSpotController.getTourSpot(id);
+                    if (!expected.containsKey(id))
+                        expected.put(id, 0L);
+                    expected.put(id, expected.get(id) + VISIT_SCORE);
+                }
+            }
+            for (int i = 0; i < 1000; i++) {
+                // 전체 중 20% - 예외 발생하여 집계되지 않음
+                // 전체 중 80% - 정상 처리되어 집계됨
+                if (Math.random() < 0.2) {
+                    try {
+                        userController.addUserBookmark("user", new AddBookmarkRequest(2000L));
+                    } catch (Exception e) {}
+                } else {
+                    // 무작위 관광지, 사용자 선정
+                    long id = (long) (Math.random() * 99) + 1;
+                    long useri = (long) (Math.random() * 9) + 1;
+                    when(userDetailsService.getCurrentContextUser()).thenAnswer(invocation ->
+                            new CustomUserDetails(null, "user"+useri, null, null, null, null)
+                    );
+                    userController.addUserBookmark("user"+useri, new AddBookmarkRequest(id));
+                    if (!expected.containsKey(id))
+                        expected.put(id, 0L);
+                    expected.put(id, expected.get(id) + BOOKMARK_SCORE);
+                }
+            }
+            
+            // 기대하는 순위 및 점수가 실제 순위 및 점수가 일치해야 한다
+            assertEquals(expected.entrySet(), collected.entrySet());
+            System.out.println("무작위 세션" + iter + " 완료");
+        }
+    }
+}
--- backend/src/test/java/com/positivewand/tourin/web/tourspot/RecommendationControllerTest.java
+++ backend/src/test/java/com/positivewand/tourin/web/tourspot/RecommendationControllerTest.java
@@ -1,6 +1,10 @@
 package com.positivewand.tourin.web.tourspot;
 
 import com.positivewand.tourin.domain.auth.CustomUserDetailsService;
+import com.positivewand.tourin.domain.recommendation.TrendService;
+import com.positivewand.tourin.domain.tourspot.TourSpotRepository;
+import com.positivewand.tourin.domain.tourspot.entity.TourSpot;
+import com.positivewand.tourin.domain.tourspot.entity.TourSpotCategory;
 import com.positivewand.tourin.domain.user.UserRepository;
 import com.positivewand.tourin.domain.user.entity.User;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +18,8 @@ import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
@@ -37,6 +43,10 @@ class RecommendationControllerTest {
     private CustomUserDetailsService customUserDetailsService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private TourSpotRepository tourSpotRepository;
+    @Autowired
+    private TrendService trendService;
 
     @BeforeEach
     void setUpEach() {
@@ -60,8 +70,8 @@ class RecommendationControllerTest {
     }
 
     @Test
-    void 로그인된_회원은_메인_추천을_받을_수_있다() throws Exception {
-        mockMvc.perform(get("/api/recommendations/main")
+    void 로그인된_회원은_테스트_추천을_받을_수_있다() throws Exception {
+        mockMvc.perform(get("/api/recommendations/test")
                         .with(testUser("userfortestA"))
                 )
                 .andExpect(status().isOk())
@@ -77,10 +87,60 @@ class RecommendationControllerTest {
     }
 
     @Test
-    void 로그인_안된_회원은_메인_추천을_받을_수_없다() throws Exception {
-        mockMvc.perform(get("/api/recommendations/main")
-                        .with(anonymous())
+    void 로그인된_회원은_트렌드_추천을_받을_수_있다() throws Exception {
+        // given - 관광지 10개
+        for (int i = 0; i < 10; i++) {
+            TourSpot testTourSpot = TourSpot.create(
+                    1000000000+i,
+                    "관광지" + i,
+                    "관광지 설명...",
+                    "대충 이미지 url",
+                    "대충 full address",
+                    "대충 address1",
+                    "대충 address2",
+                    111,
+                    111,
+                    37.5642135,
+                    127.0016985,
+                    Arrays.asList(TourSpotCategory.EXPERIENCE)
+            );
+
+            tourSpotRepository.save(testTourSpot);
+        }
+
+        // given - 트렌드 점수 저장소에 관광지 저장
+        trendService.resetTrendScore();
+        for (int id = 1000000000; id < 1000000010; id++) {
+            trendService.incrementTrendScore(id, 1);
+        }
+
+        mockMvc.perform(get("/api/recommendations/trend")
+                        .with(testUser("userfortestA"))
                 )
-                .andExpect(status().isUnauthorized());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[*].id").exists())
+                .andExpect(jsonPath("$[*].name").exists())
+                .andExpect(jsonPath("$[*].address").exists())
+                .andExpect(jsonPath("$[*].lat").exists())
+                .andExpect(jsonPath("$[*].lng").exists())
+                .andExpect(jsonPath("$[*].imageUrl").exists())
+                .andExpect(jsonPath("$[*].description").exists())
+                .andExpect(jsonPath("$[*].phoneNumber").exists())
+                .andExpect(jsonPath("$[*].tags").exists());
+    }
+
+    @Test
+    void 로그인_안된_회원은_추천을_받을_수_없다() throws Exception {
+        String[] endpoints = {
+                "/api/recommendations/trend",
+                "/api/recommendations/test"
+        };
+
+        for (String endpoint: endpoints) {
+            mockMvc.perform(get(endpoint)
+                            .with(anonymous())
+                    )
+                    .andExpect(status().isUnauthorized());
+        }
     }
 }
