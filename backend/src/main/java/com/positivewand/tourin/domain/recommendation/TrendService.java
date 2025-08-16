1833acfbead5750e5b104aaa352e348c7e0f2fd2
positiveWand <singbhang@gmail.com>
2025-08-16 22:51:48 +0900

    refactor/backend: Redis Config 패키지 옮김
    인프라 관련 패키지를 만들어 옮김

--- backend/src/main/java/com/positivewand/tourin/domain/recommendation/TrendService.java
+++ backend/src/main/java/com/positivewand/tourin/domain/recommendation/TrendService.java
@@ -1,21 +1,17 @@
 package com.positivewand.tourin.domain.recommendation;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.ZSetOperations;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.stream.Collectors;
-
-import static com.positivewand.tourin.domain.RedisConfig.TREND_TOPK_KEY;
 
 @Service
 @RequiredArgsConstructor
 public class TrendService {
     private final RedisOperations<String, String> topkOperations;
+
+    private static final String TREND_TOPK_KEY = "recommendation:trend:topk";
 
     public void resetTrendScore() {
         topkOperations.delete(TREND_TOPK_KEY);
