fd7bac58174cc3e867a88761211be620dd699bc2
positiveWand <singbhang@gmail.com>
2025-10-08 18:33:51 +0900

    feat/frontend: 메인페이지 추천 컴포넌트
    메인페이지에 추천 관광지(트렌드 추천, 방문/북마크 기록 기반 개인 추천)를 제시하는 기능

--- frontend/src/app/main-page/main-recommendation.tsx
+++ frontend/src/app/main-page/main-recommendation.tsx
@@ -5,33 +5,38 @@ import { cn } from '@/lib/utils';
 
 export interface MainRecommendationProps {
   className?: string;
+  type: string
+  placeholder: string
 }
 
-export function MainRecommendation({ className }: MainRecommendationProps) {
-  const [recommendation, error, loading] = useTourSpotRecommendations('trend');
+export function Recommendation({ className, type, placeholder }: MainRecommendationProps) {
+  const [recommendation, error, loading] = useTourSpotRecommendations(type);
 
   if (loading) {
     return null;
   }
 
   return (
-    <div
-      className={cn(
-        'grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-2 justify-items-center',
-        className,
-      )}
-    >
-      {recommendation.map((tourSpot) => {
-        return (
-          <TourSpotCard
-            name={tourSpot.name}
-            address={tourSpot.address}
-            imageUrl={tourSpot.imageUrl}
-            tags={tourSpot.tags}
-            href={MAP_PAGE + `/tourSpot/${tourSpot.id}`}
-          />
-        );
-      })}
-    </div>
+      recommendation.length != 0 ?
+        <div
+          className={cn(
+            'grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-2 justify-items-center',
+            className,
+          )}
+        >
+        {recommendation.map((tourSpot) => {
+          return (
+            <TourSpotCard
+              name={tourSpot.name}
+              address={tourSpot.address}
+              imageUrl={tourSpot.imageUrl}
+              tags={tourSpot.tags}
+              href={MAP_PAGE + `/tourSpot/${tourSpot.id}`}
+            />
+          );
+        })}
+        </div>
+      :
+      <div className='text-center text-xl text-muted-foreground my-10'>{placeholder}</div>
   );
 }
