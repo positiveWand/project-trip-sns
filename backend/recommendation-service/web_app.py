ccef9836fd8dd7e8d6d166e7b2d78751094655e9
positiveWand <singbhang@gmail.com>
2025-10-08 18:17:32 +0900

    feat/backend: recommendation-service 개인화 추천 웹 API

--- backend/recommendation-service/web_app.py
+++ backend/recommendation-service/web_app.py
@@ -3,6 +3,7 @@ load_dotenv('./config.env')
 
 from fastapi import FastAPI, Query
 import service.trend as trend_service
+import service.personalized as personalized_service
 
 app = FastAPI()
 
@@ -11,4 +12,12 @@ async def get_trend_recommendation(
     k: int = Query(5, ge=0)
 ):
     topk = trend_service.get_trend_topk(k)
+    return topk
+
+@app.get('/api/recommendations/personalized')
+async def get_personalized_recommendation(
+    user_id: str,
+    k: int = Query(5, ge=0)
+):
+    topk = personalized_service.get_personalized_topk(user_id, k)
     return topk
